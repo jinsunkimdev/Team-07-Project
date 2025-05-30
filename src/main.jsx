9041511cc0cc9cb8e44c3ac4e57df0d529f5bbfe
Jinsun Kim <jinsunkim94@gmail.com>
2025-05-30 17:06:06 +0900

    style: 전역 스타일 구조 및 변수 최적화및 폰트 관련 변수 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Reset } from "./styles/Reset.jsx";
+import { Variables } from "./styles/Variables.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Reset />
+    <Variables />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
