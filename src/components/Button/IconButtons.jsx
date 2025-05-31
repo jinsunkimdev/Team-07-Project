db5f3433cb8b3bf78cd18452a61c776d109f06a4
10000jaeN <akswo256@naver.com>
2025-05-31 13:44:38 +0900

    refactor: iconButtons에 onClick 추가 및 설명

--- src/components/Button/IconButtons.jsx
+++ src/components/Button/IconButtons.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-/** 제발려 */
+import { css } from "@emotion/react";
+
 import iconPlus from "../../../assets/iconPlus.svg";
 import iconDelete from "../../../assets/iconDeleted.svg";
 import iconArrowLeft from "../../../assets/iconArrowLeft.svg";
@@ -7,38 +8,53 @@ import iconArrowRight from "../../../assets/iconArrowRight.svg";
 import iconCheck from "../../../assets/iconCheck.svg";
 import iconShare20 from "../../../assets/iconShare20.svg";
 import iconShare24 from "../../../assets/iconShare24.svg";
-import { css } from "@emotion/react";
 
-export const IconPlusButton = ({ ...props }) => (
-  <button css={plusButton} {...props}>
-    <img src={iconPlus} alt="Plus button" />
-  </button>
-);
+import { useNavigate } from "react-router-dom";
 
-export const IconDeleteButton = ({ onClick, ...props }) => (
+export const IconPlusButton = ({ to, ...props }) => {
+  const navigate = useNavigate(); //to 로 경로만 설정해주면 onClick으로 이동
+
+  return (
+    <button css={plusButton} onClick={() => navigate(to)} {...props}>
+      <img src={iconPlus} alt="Plus button" />
+    </button>
+  );
+};
+
+export const IconDeleteButton = (
+  { onClick, ...props } //리스트 로직 보고 판단.
+) => (
   <button css={deleteButton} onClick={onClick} {...props}>
     <img src={iconDelete} alt="delete button" />
   </button>
 );
 
-export const IconArrowLeftButton = () => (
-  <button css={arrowButton}>
-    <img src={iconArrowLeft} alt="ArrowLeft button" />
-  </button>
-);
+export const ScrollArrowButton = ({
+  direction = left, //dirction 으로 알맞은 방향 표시
+  onClick,
+  visible = true, //visible 프롭은 boolean으로 받고, false일 경우 null을 반환, 렌더x
+  ...props
+}) => {
+  if (!visible) return null;
 
-export const IconArrowRightButton = () => (
-  <button css={arrowButton}>
-    <img src={iconArrowRight} alt="ArrowRight button" />
-  </button>
-);
+  const iconSrc = direction === left ? iconArrowLeft : iconArrowRight;
+  const altText = direction === left ? "scroll left" : "scroll right";
+
+  return (
+    <button css={arrowButton} onClick={onClick} {...props}>
+      <img src={iconSrc} alt={altText} />
+    </button>
+  );
+};
 
 export const IconCheckButton = () => (
+  //로직 보고 판단.
   <button css={checkButton}>
     <img src={iconCheck} alt="Check button" />
   </button>
 );
 
+//아래 두 버튼은 추후에 합쳐서 반응형으로 만들지, 각자 렌더링할지 고민중
 export const IconShare20Button = () => (
   <button css={shareButton}>
     <img src={iconShare20} alt="Share button" />
