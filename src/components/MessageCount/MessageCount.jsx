1211f00145804a689f3596f88da201959f38f4e1
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-01 11:58:32 +0900

    feat: MessageCount 컴포넌트 추가 (ListPageHeader 자식 요소)

---
+++ src/components/MessageCount/MessageCount.jsx
@@ -0,0 +1,21 @@
+import { css } from "@emotion/react";
+import AvatarList from "../Avatar/AvatarList";
+
+const MessageCount = ({ items }) => {
+  return (
+    <div css={MessageCountStyle}>
+      <AvatarList items={items} />
+      <span>
+        <b>{items.length}</b>명이 작성했어요!
+      </span>
+    </div>
+  );
+};
+
+export default MessageCount;
+
+const MessageCountStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
