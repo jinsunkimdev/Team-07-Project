55948b101931f8113a1b8ad9dcd20a48a85ea164
leesangdal <asksa1256@gmail.com>
2025-06-01 22:36:00 +0900

    refactor: AddMessageCard → AddMessageCardButton으로 컴포넌트명 변경 및 스타일 파일 분리 (vite hmr warning)

--- src/components/MessageCard/MessageCard.jsx
+++ src/components/MessageCard/MessageCard.jsx
@@ -1,9 +1,9 @@
-import { css } from "@emotion/react";
-import { BREAKPOINTS } from "../../constants/constants";
+import MessageCardStyle from "./MessageCardStyle";
 import Avatar from "../Avatar";
 import { IconDeleteButton } from "./../Button/IconButtons";
+import formatDate from "../../utils/formatDate";
 
-const MessageCard = ({ messageData = {}, isRecipient = false }) => {
+const MessageCard = ({ messageData = {}, isEditable = false }) => {
   const {
     sender,
     profileImg,
@@ -12,8 +12,12 @@ const MessageCard = ({ messageData = {}, isRecipient = false }) => {
     font = "Pretendard",
     createdAt,
   } = messageData;
+
   return (
-    <div css={MessageCardStyle}>
+    <div
+      css={MessageCardStyle}
+      onClick={() => alert("CardViewModal will be opened")}
+    >
       <div className="card-header">
         <div className="sender-profile-wrap">
           <Avatar imgSrc={profileImg} size="md" />
@@ -27,86 +31,16 @@ const MessageCard = ({ messageData = {}, isRecipient = false }) => {
             </span>
           </div>
         </div>
-        {isRecipient && <IconDeleteButton />}
+        {isEditable && <IconDeleteButton onClick={() => alert("delete")} />}
       </div>
       <div className="card-body">
         <p className="content">{content}</p>
       </div>
       <div className="card-footer">
-        <span className="date">{createdAt}</span>
+        <span className="date">{formatDate(createdAt)}</span>
       </div>
     </div>
   );
 };
 
 export default MessageCard;
-
-export const MessageCardStyle = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  height: 230px;
-  padding: 28px 24px 24px;
-  background-color: var(--white);
-  border-radius: var(--radius-lg);
-  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.08);
-  word-break: keep-all;
-  cursor: pointer;
-
-  @media (min-width: ${BREAKPOINTS.md}px) {
-    max-width: calc(50% - 16px);
-    height: 284px;
-  }
-
-  @media (min-width: ${BREAKPOINTS.lg}px) {
-    max-width: 384px;
-    height: 280px;
-    overflow: hidden;
-  }
-
-  .card-header {
-    display: flex;
-    justify-content: space-between;
-    padding-bottom: 16px;
-    border-bottom: 1px solid var(--gray-200);
-  }
-
-  .sender-profile-wrap {
-    display: flex;
-    gap: 14px;
-
-    .sender-profile {
-      .name {
-        display: block;
-        font-size: var(--font-size-20);
-      }
-    }
-  }
-
-  .card-body {
-    height: 100%;
-  }
-
-  .content {
-    height: 56px;
-    margin-bottom: 16px;
-    padding-top: 16px;
-    font-size: var(--font-size-15);
-    color: var(--gray-600);
-    display: -webkit-box;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-
-    @media (min-width: ${BREAKPOINTS.md}px) {
-      height: 100px;
-      font-size: var(--font-size-18);
-    }
-  }
-
-  .date {
-    font-size: var(--font-size-12);
-    color: var(--gray-400);
-  }
-`;
---
+++ src/components/MessageCard/MessageCardStyle.js
@@ -0,0 +1,72 @@
+import { css } from "@emotion/react";
+import { BREAKPOINTS } from "../../constants/constants";
+
+const MessageCardStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 230px;
+  padding: 28px 24px 24px;
+  background-color: var(--white);
+  border-radius: var(--radius-lg);
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.08);
+  word-break: keep-all;
+  cursor: pointer;
+
+  @media (min-width: ${BREAKPOINTS.md}px) {
+    height: 284px;
+  }
+
+  @media (min-width: ${BREAKPOINTS.lg}px) {
+    height: 280px;
+    overflow: hidden;
+  }
+
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 16px;
+    border-bottom: 1px solid var(--gray-200);
+  }
+
+  .sender-profile-wrap {
+    display: flex;
+    gap: 14px;
+
+    .sender-profile {
+      .name {
+        display: block;
+        font-size: var(--font-size-20);
+      }
+    }
+  }
+
+  .card-body {
+    height: 100%;
+  }
+
+  .content {
+    height: 56px;
+    margin-bottom: 16px;
+    padding-top: 16px;
+    font-size: var(--font-size-15);
+    color: var(--gray-600);
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+
+    @media (min-width: ${BREAKPOINTS.md}px) {
+      height: 100px;
+      font-size: var(--font-size-18);
+    }
+  }
+
+  .date {
+    font-size: var(--font-size-12);
+    color: var(--gray-400);
+  }
+`;
+
+export default MessageCardStyle;
