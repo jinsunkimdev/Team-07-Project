c237f2f2e046dce7e584b6658d14a68c2cb58042
leesangdal <asksa1256@gmail.com>
2025-06-01 14:38:22 +0900

    refactor: MessageCount → MessageAuthors로 컴포넌트병 변경

--- src/components/MessageAuthors/MessageAuthors.jsx
+++ src/components/MessageAuthors/MessageAuthors.jsx
@@ -1,11 +1,11 @@
 import { css } from "@emotion/react";
 import AvatarList from "../Avatar/AvatarList";
 
-const MessageCount = ({ items }) => {
+const MessageAuthors = ({ items }) => {
   if (!items || items.length === 0) return null;
 
   return (
-    <div css={MessageCountStyle}>
+    <div css={MessageAuthorsStyle}>
       <AvatarList items={items} />
       <span>
         <b>{items.length}</b>명이 작성했어요!
@@ -14,9 +14,9 @@ const MessageCount = ({ items }) => {
   );
 };
 
-export default MessageCount;
+export default MessageAuthors;
 
-const MessageCountStyle = css`
+const MessageAuthorsStyle = css`
   display: flex;
   align-items: center;
   gap: 10px;
