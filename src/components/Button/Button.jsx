a88b5c7a4cd7ba8a8d11498bb6c90647ba73b346
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-02 14:02:43 +0900

    refactor: Button, AddEmojiButton 스타일 코드 정리 (Button.styles.js ➡️ btnStylesMap.js)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,43 @@
-import { StyledButton } from "./Button.styles";
+import { css } from "@emotion/react";
+import { btnStyles } from "./btnStylesMap";
 
-const Button = ({ variant = "primary", children, ...props }) => (
-  <StyledButton variant={variant} {...props}>
+const Button = ({
+  variant = "primary",
+  size = "md",
+  children,
+  disabled,
+  onClick,
+}) => (
+  <button
+    css={ButtonStyle({ size, variant, disabled })}
+    disabled={disabled}
+    onClick={onClick}
+  >
     {children}
-  </StyledButton>
+  </button>
 );
 
 export default Button;
+
+const getVariantStyle = (variant, disabled) =>
+  disabled
+    ? css`
+        ${btnStyles.variants.disabled}
+      `
+    : css`
+        ${btnStyles.variants[variant]}
+      `;
+
+export const ButtonStyle = (props) => css`
+  ${getVariantStyle(props.variant, props.disabled)};
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: ${btnStyles.minWidth[props.size]};
+  height: ${btnStyles.height[props.size]};
+  padding: ${btnStyles.padding[props.size]};
+  font-size: ${btnStyles.fontSize[props.size]};
+  font-weight: ${btnStyles.fontWeight[props.size]};
+  border-radius: ${btnStyles.radius[props.size]};
+`;
