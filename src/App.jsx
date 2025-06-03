d65ba3f80f1b1a952ada710128c70912a080b9c5
leesangdal <asksa1256@gmail.com>
2025-06-03 19:33:08 +0900

    feat: 페이지 전환 시에도 토스트 유지하기 (구현중...)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import Router from "./routes/Router";
-import ToastProvider from "./components/Toast/ToastProvider";
 
 function App() {
-  return (
-    <>
-      <ToastProvider>
-        <Router />
-      </ToastProvider>
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Reset } from "./styles/Reset.jsx";
 import { Variables } from "./styles/Variables.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ToastProvider from "./components/Toast/ToastProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Reset />
-    <Variables />
-    <App />
+    <ToastProvider>
+      <Reset />
+      <Variables />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToastProvider>
   </StrictMode>
 );
