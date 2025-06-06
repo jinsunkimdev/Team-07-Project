dbee4f8f106a28bd1912edbd77ae4cf84a026a37
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-06 14:28:38 +0900

    fix: 드롭다운에 option.'value'로 전달되도록 constants 공유 프로퍼티명 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,12 +9,12 @@ export const TEAM = "7";
 export const SHARE_DROPDOWN_ITEMS = [
   {
     label: "카카오톡 공유",
-    successMsg: "롤링페이퍼를 카카오톡으로 공유합니다.",
+    value: "롤링페이퍼를 카카오톡으로 공유합니다.",
     errorMsg: "카카오톡 공유에 실패했어요..🥲",
   },
   {
     label: "URL 복사",
-    successMsg: "URL이 복사되었습니다.",
+    value: "URL이 복사되었습니다.",
     errorMsg: "URL 복사에 실패했어요..🥲",
   },
 ];
