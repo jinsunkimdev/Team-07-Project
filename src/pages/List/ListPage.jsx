a1476cc3905e403bf9b9e17c3ff50a185942a573
youjin-hub <leeyoujin0704@naver.com>
2025-06-07 00:23:17 +0900

    feat: header에 롤링페이퍼 만들기 버튼 추가(스타일 간소화)

--- src/pages/List/ListPage.jsx
+++ src/pages/List/ListPage.jsx
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
 import GlobalHeader from "../../components/Header/GlobalHeader";
-import ListPageHeader from "./ListPageHeader";
+import { css } from "@emotion/react";
+import { BREAKPOINTS } from "../../constants/constants";
 
 function ListPage() {
   return (
     <div>
-      <GlobalHeader />
-      <ListPageHeader recipient="Ashley Kim" />
-      ListPage
+      <GlobalHeader>
+        <Button
+          as={Link}
+          to="/post"
+          variant="outlined"
+          size="md"
+          css={css`
+            width: auto;
+            @media (max-width: ${BREAKPOINTS.md}px) {
+              font-size: var(--font-size-14);
+            }
+          `}
+        >
+          롤링 페이퍼 만들기
+        </Button>
+      </GlobalHeader>
     </div>
   );
 }
