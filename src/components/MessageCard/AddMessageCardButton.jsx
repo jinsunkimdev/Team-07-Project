3836bc57399e825036c336503c8ca11a182e5032
Jinsun Kim <jinsunkim94@gmail.com>
2025-06-08 01:42:14 +0900

    fix(nav): useParams로 recipient id 동적 처리

--- src/components/MessageCard/AddMessageCardButton.jsx
+++ src/components/MessageCard/AddMessageCardButton.jsx
@@ -1,13 +1,14 @@
 import { css } from "@emotion/react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import MessageCardStyle from "./MessageCardStyle";
 import { IconPlusButton } from "../Button/IconButtons";
 
 const AddMessageCardButton = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const goToPostPage = () => {
-    navigate("/post");
+    navigate(`/post/${id}/message`);
   };
 
   return (
