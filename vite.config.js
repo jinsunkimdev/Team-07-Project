521ac161ca0184fc8c2e3bc9e311d6103a9679bd
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-08 18:38:42 +0900

    chore: github pages 배포 재설정 (경로 변경)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Team-07-Rolling-Github-Pages/",
+  base: "/rolling/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
