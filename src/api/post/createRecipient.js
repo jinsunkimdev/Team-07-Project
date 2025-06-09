8c4df0c33c3795d90a1547585b31270842eb27c4
leesangdal <asksa1256@gmail.com>
2025-06-09 15:31:39 +0900

    fix(createRecipient): res.json() 앞에 await 빼먹음

---
+++ src/api/post/createRecipient.js
@@ -0,0 +1,20 @@
+const createRecipient = async (data) => {
+  const url = `https://rolling-api.vercel.app/16-7/recipients/`;
+  console.log(data);
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error("롤링페이퍼 생성에 실패했어요. 😥");
+  }
+
+  const resData = await res.json();
+
+  return resData;
+};
+
+export default createRecipient;
