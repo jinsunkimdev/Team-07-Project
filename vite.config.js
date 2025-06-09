30508bd60ca43c269ea00de54757ac0724115272
Jinsun Kim <jinsunkim94@gmail.com>
2025-06-09 17:00:40 +0900

    test:워크플로우 실행 확인용

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-// vite 설정 파일입니다.
+// vite 설정파일
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base url
-  // vercel용 base url입니다.
   base: "/",
   plugins: [
     // @emotion/react를 자동 임포트 해줍니다.
-    // 일일이 적어주지 않아도 됩니다.
     react({
       jsxImportSource: "@emotion/react",
     }),
