ac2bd9c3d33fee08991cd4a508d4e589abde1bd0
leesangdal <asksa1256@gmail.com>
2025-06-10 21:40:07 +0900

    feat(SelectBackground): SkeletonImage 컴포넌트 추가 + Suspense로 스켈레톤 구현

---
+++ src/components/Skeleton/SkeletonImage.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import Skeleton from "./Skeleton";
+
+const SkeletonImage = ({ src, borderRadius }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => setLoaded(true);
+  }, [src]);
+
+  return loaded ? (
+    <img src={src} alt="" />
+  ) : (
+    <Skeleton borderRadius={borderRadius} />
+  );
+};
+
+export default SkeletonImage;
