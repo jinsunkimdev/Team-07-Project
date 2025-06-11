a7115ae43f5ef879a9a3921818675bb6e01bded2
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-11 11:27:59 +0900

    feat: 페이지 이동 시 스크롤 최상단으로 이동 (무한스크롤 페이지, 리스트 페이지 제외)

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { css } from "@emotion/react";
 import Router from "./routes/Router";
 import LoadingPage from "./pages/LoadingPage";
 import usePageLoadTransition from "./hooks/usePageLoadTransition";
+import useScrollToTop from "./hooks/useScrollToTop";
 
 function App() {
   const { isPageLoading, showLoadingUI, fadeIn } = usePageLoadTransition();
+  useScrollToTop();
 
   if (isPageLoading) {
     return <LoadingPage showLoading={showLoadingUI} />;
